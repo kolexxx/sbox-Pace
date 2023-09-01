09f2b87d22889ec40318a4a40176158d56d81cf1
Kole <novoselkosta@gmail.com>
2023-09-01 14:26:43 +0200

    Weapon pickups replenish ammo if we already have the weapon

--- code/Hammer/WeaponPickup.cs
+++ code/Hammer/WeaponPickup.cs
@@ -11,46 +11,54 @@ public partial class WeaponPickup : ModelEntity
 	[Net, Property] public WeaponDefinition Weapon { get; set; }
 	[Net] private TimeUntil TimeUntilRespawn { get; set; } = 0f;
 
-	private SceneObject _sceneModel;
+	private SceneObject _sceneObject;
 
 	public override void Spawn()
 	{
 		SetupPhysicsFromAABB( PhysicsMotionType.Static, new Vector3( -1, -40, 0 ), new Vector3( 1, 40, 80 ) );
 
 		Tags.Add( "trigger" );
-
-		EnableAllCollisions = true;
+		EnableTouch = true;
 		EnableTouchPersists = true;
 	}
 
 	public override void ClientSpawn()
 	{
-		_sceneModel = new( Game.SceneWorld, Weapon.Model, new Transform( Position, Rotation, 1.2f ) );
+		_sceneObject = new( Game.SceneWorld, Weapon.Model, new Transform( Position, Rotation, 1.2f ) );
 	}
 
 	public override void Touch( Entity other )
 	{
-		Log.Info( $"{other} has touched a pickup" );
-
 		if ( !Game.IsServer || other is not Pawn player || !TimeUntilRespawn )
 			return;
 
 		if ( player.Inventory[Weapon.Slot] is not null )
-			return;
+		{
+			var weapon = player.Inventory[Weapon.Slot];
 
-		TimeUntilRespawn = 10f;
-		player.Inventory.Pickup( TypeLibrary.Create<Weapon>( Weapon.ClassName ) );
+			if ( weapon.ReserveAmmo >= 2 * weapon.Definition.ClipSize )
+				return;
+
+			TimeUntilRespawn = 10f;
+			weapon.ReserveAmmo = 2 * weapon.Definition.ClipSize;
+			Sound.FromEntity( "pickup_weapon", player );
+		}
+		else
+		{
+			TimeUntilRespawn = 10f;
+			player.Inventory.Pickup( TypeLibrary.Create<Weapon>( Weapon.ClassName ) );
+		}
 	}
 
 	[GameEvent.Client.Frame]
 	private void Tick()
 	{
-		_sceneModel.RenderingEnabled = TimeUntilRespawn;
+		_sceneObject.RenderingEnabled = TimeUntilRespawn;
 
-		if ( !_sceneModel.RenderingEnabled )
+		if ( !_sceneObject.RenderingEnabled )
 			return;
 
-		_sceneModel.Position = Position + (MathF.Sin( RealTime.Now * 2.5f ) * 10f + 45f) * Vector3.Up;
-		_sceneModel.Rotation = Rotation.FromAxis( Vector3.Up, RealTime.Now * 100f );
+		_sceneObject.Position = Position + (MathF.Sin( RealTime.Now * 2.5f ) * 10f + 45f) * Vector3.Up;
+		_sceneObject.Rotation = Rotation.FromAxis( Vector3.Up, RealTime.Now * 100f );
 	}
 }
